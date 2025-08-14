a6fe42dabc3ed52ba1743d2b77a5d7dc294496b0
Nhat-Tien Nguyen <nhattn38@gmail.com>
2025-08-14 17:32:11 -0500

    Updated terraform resource name for S3 to be more specific

--- infra/environments/modules/s3/main.tf
+++ infra/environments/modules/s3/main.tf
@@ -1,21 +1,24 @@
 # modules/s3/main.tf  (relative: infra/environments/modules/s3/main.tf)
 
-resource "aws_s3_bucket" "site" {
+resource "aws_s3_bucket" "resumebucket" {
   bucket = var.bucket_name
    tags = {
     Project = "Cloud Resume Challenge"
   }
+    lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "aws_s3_bucket_public_access_block" "pab" {
-  bucket                  = aws_s3_bucket.site.id
+resource "aws_s3_bucket_public_access_block" "resumebucket_pab" {
+  bucket                  = aws_s3_bucket.resumebucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "own" {
-  bucket = aws_s3_bucket.site.id
+resource "aws_s3_bucket_ownership_controls" "resumebucket_own" {
+  bucket = aws_s3_bucket.resumebucket.id
   rule { object_ownership = "BucketOwnerEnforced" } 
 }
--- infra/environments/modules/s3/outputs.tf
+++ infra/environments/modules/s3/outputs.tf
@@ -1,11 +1,11 @@
 output "bucket_id" {
-    value = aws_s3_bucket.site.id 
+    value = aws_s3_bucket.resumebucket.id
 }
 
 output "bucket_arn" { 
-    value = aws_s3_bucket.site.arn
+    value = aws_s3_bucket.resumebucket.arn
 }
 
 output "bucket_domain_name" {
-    value = aws_s3_bucket.site.bucket_regional_domain_name
+    value = aws_s3_bucket.resumebucket.bucket_regional_domain_name
 }
