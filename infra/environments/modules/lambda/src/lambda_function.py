b7b384abdf51550f611e153489c795f1eb22d101
Nhat-Tien Nguyen <nhattn38@gmail.com>
2025-09-07 16:21:17 -0500

    Update CORS policy to restrict access to specific origin instead of *

--- infra/environments/modules/lambda/src/lambda_function.py
+++ infra/environments/modules/lambda/src/lambda_function.py
@@ -27,7 +27,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': 'https://tien-cloud.com'
         },
         'body': json.dumps({'pageViews': pageViews})
     }
